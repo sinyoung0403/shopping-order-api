d9e8272a0e21e978e9fdc16f4a0af7df1a3b8cbf
sinyoung0403 <tlstjs753@gmail.com>
2025-08-04 21:19:35 +0900

    ✨ [Feat] Global Exception Handler 구현

---
+++ src/main/java/com/shoppingorderapi/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.shoppingorderapi.common.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.shoppingorderapi.common.response.BaseResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	/**
+	 * 비즈니스 예외 처리
+	 */
+	@ExceptionHandler(CustomException.class)
+	public ResponseEntity<BaseResponse<Void>> handleCustomException(CustomException ex) {
+		ErrorCode errorCode = ex.getErrorCode();
+
+		return ResponseEntity
+			.status(errorCode.getStatus())
+			.body(BaseResponse.error(
+				errorCode.getMessage(),
+				errorCode.getCode()
+			));
+	}
+
+	/**
+	 * 예상하지 못한 예외 처리
+	 */
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<BaseResponse<Void>> handleException(Exception ex, HttpServletRequest request) {
+		ex.printStackTrace();
+
+		return ResponseEntity
+			.status(HttpStatus.INTERNAL_SERVER_ERROR)
+			.body(BaseResponse.error(
+				"서버 내부 오류가 발생했습니다.",
+				"INTERNAL_SERVER_ERROR"
+			));
+	}
+}
