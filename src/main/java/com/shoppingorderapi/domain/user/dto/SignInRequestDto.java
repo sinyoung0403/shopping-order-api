0341679d671b8f64c6dd4a78300352e992bb578b
sinyoung0403 <tlstjs753@gmail.com>
2025-08-04 16:49:20 +0900

    ✨ [Feat] DTO 유효성 검증 어노테이션 추가

--- src/main/java/com/shoppingorderapi/domain/user/dto/SignInRequestDto.java
+++ src/main/java/com/shoppingorderapi/domain/user/dto/SignInRequestDto.java
@@ -1,9 +1,16 @@
 package com.shoppingorderapi.domain.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 import lombok.Getter;
 
 @Getter
 public class SignInRequestDto {
+	@NotBlank(message = "이메일은 필수입니다")
+	@Email(message = "올바른 이메일 형식이 아닙니다")
 	private String email;
+
+	@NotBlank(message = "비밀번호는 필수입니다")
 	private String password;
 }
