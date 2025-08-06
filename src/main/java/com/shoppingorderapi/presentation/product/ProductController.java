daaaf210271daae63d18f6c33af76be09afb1884
sinyoung0403 <tlstjs753@gmail.com>
2025-08-06 19:19:48 +0900

    ✨ [Feat] Product Create Controller 구현

---
+++ src/main/java/com/shoppingorderapi/presentation/product/ProductController.java
@@ -0,0 +1,38 @@
+package com.shoppingorderapi.presentation.product;
+
+import jakarta.validation.Valid;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shoppingorderapi.application.product.ProductService;
+import com.shoppingorderapi.common.response.BaseResponse;
+import com.shoppingorderapi.domain.product.dto.CreateProductRequest;
+import com.shoppingorderapi.domain.product.dto.CreateProductResponse;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductController {
+
+	private final ProductService productService;
+
+	/**
+	 * 상품의 생성
+	 *
+	 * @param createProductRequest 상품 생성 요청 정보
+	 * @return 생성된 상품 정보와 함께 201 Created 응답
+	 */
+	@PostMapping("/product")
+	public ResponseEntity<BaseResponse<CreateProductResponse>> createProduct(
+		@Valid @RequestBody CreateProductRequest createProductRequest
+	) {
+		return ResponseEntity
+			.status(HttpStatus.CREATED)
+			.body(BaseResponse.success(productService.createProduct(createProductRequest)));
+	}
+}
