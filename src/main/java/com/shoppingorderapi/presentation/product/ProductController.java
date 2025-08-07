b317e01d816c1639145ed7e8bc797fde5f69a907
sinyoung0403 <tlstjs753@gmail.com>
2025-08-07 20:34:45 +0900

    ✨ [Feat] 상품 삭제 API 메서드 구현

--- src/main/java/com/shoppingorderapi/presentation/product/ProductController.java
+++ src/main/java/com/shoppingorderapi/presentation/product/ProductController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -70,6 +71,16 @@ public class ProductController {
 		@RequestBody UpdateProductRequestDto updateProductRequestDto
 	) {
 		productService.updateProduct(productId, updateProductRequestDto);
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.body(BaseResponse.success(null));
+	}
+
+	@DeleteMapping("/product/{productId}")
+	public ResponseEntity<BaseResponse<Void>> deleteProduct(
+		@PathVariable Long productId
+	) {
+		productService.deleteProduct(productId);
 		return ResponseEntity
 			.status(HttpStatus.OK)
 			.body(BaseResponse.success(null));
