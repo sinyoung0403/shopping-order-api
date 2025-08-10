d4086e677ae3b53b03df39253f4965c91728f9ba
sinyoung0403 <tlstjs753@gmail.com>
2025-08-10 21:12:28 +0900

    ✨ [Feat] Cart 조회 API 구현

--- src/main/java/com/shoppingorderapi/application/cart/CartService.java
+++ src/main/java/com/shoppingorderapi/application/cart/CartService.java
@@ -41,6 +41,32 @@ public class CartService {
 
 	public void findCart() {
 		// Cart Item 추가 -> DTO 만든 후 다시 수정할 예정
+	@Transactional(readOnly = true)
+	public CartDetailResponseDto findCart(Long userId) {
+		// 1. UserId 에 맞는 카트 조회
+		Cart cart = cartRepository.findByUser_Id(userId).orElseThrow(
+			() -> new CustomException(ErrorCode.NOT_FOUND)
+		);
+
+		// 2. CartItem 가져오기
+		List<CartItemResponseDto> itemList = cartItemRepository.findCartItemListByCartIdDto(cart.getId());
+
+		// 3. 아이템 개수
+		int itemCount = itemList.size();
+
+		// 4. 수량 합
+		int totalQuantity = itemList.stream()
+			.mapToInt(i -> Optional.ofNullable(i.getQuantity()).orElse(0))
+			.sum();
+
+		// 5. 총 가격
+		int totalAmount = itemList.stream()
+			.mapToInt(i -> Optional.ofNullable(i.getPrice()).orElse(0)
+				* Optional.ofNullable(i.getQuantity()).orElse(0))
+			.sum();
+
+		// 6. 반환
+		return CartDetailResponseDto.of(cart.getId(), itemCount, totalQuantity, totalAmount, itemList);
 	}
 
 	@Transactional
--- src/main/java/com/shoppingorderapi/presentation/cart/CartController.java
+++ src/main/java/com/shoppingorderapi/presentation/cart/CartController.java
@@ -6,14 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shoppingorderapi.application.cart.CartService;
 import com.shoppingorderapi.common.response.BaseResponse;
-import com.shoppingorderapi.domain.cart.dto.CreateCartResponseDto;
+import com.shoppingorderapi.domain.cart.dto.response.CartDetailResponseDto;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,24 +20,13 @@ public class CartController {
 	private final CartService cartService;
 
 	// TODO: 추후 Security 도입 후 user Id 토큰 속에서 받아오기
-	@PostMapping("/cart")
-	public ResponseEntity<BaseResponse<CreateCartResponseDto>> createCart(
-		@RequestParam Long userId
-	) {
-		return ResponseEntity
-			.status(HttpStatus.CREATED)
-			.body(BaseResponse.success(cartService.createCart(userId)));
-	}
-
-	// TODO: 추후 Security 도입 후 user Id 토큰 속에서 받아오기
-	// TODO: 추후 CartItem 생성 후 수정
-	@GetMapping("/cart")
-	public ResponseEntity<BaseResponse<Void>> getCart(
+	@GetMapping("/me/carts")
+	public ResponseEntity<BaseResponse<CartDetailResponseDto>> getCartItem(
 		@RequestParam Long userId
 	) {
 		return ResponseEntity
 			.status(HttpStatus.OK)
-			.body(BaseResponse.success(null));
+			.body(BaseResponse.success(cartService.findCart(userId)));
 	}
 
 	@DeleteMapping("/cart")
