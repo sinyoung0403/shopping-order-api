759676558f3a725646d2d3ea61ff645d3cd661c6
sinyoung0403 <tlstjs753@gmail.com>
2025-08-10 21:30:06 +0900

    ♻️ [Refactor] wrapper(Integer)로 전환

--- src/main/java/com/shoppingorderapi/domain/cartItem/dto/request/CreateCartItemRequestDto.java
+++ src/main/java/com/shoppingorderapi/domain/cartItem/dto/request/CreateCartItemRequestDto.java
@@ -1,5 +1,6 @@
 package com.shoppingorderapi.domain.cartItem.dto.request;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
@@ -14,5 +15,6 @@ public class CreateCartItemRequestDto {
 
 	@NotNull(message = "상품 수량은 필수 입력 값입니다.")
 	@Positive(message = "상품 수량은 1 이상의 값이어야 합니다.")
-	private int quantity;
+	@Max(value = 100, message = "상품 수량은 최대 100까지 가능합니다.")
+	private Integer quantity;
 }
