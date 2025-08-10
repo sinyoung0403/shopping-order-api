f2905101e9bf62a05bf6c19fc37f7225384f5d23
sinyoung0403 <tlstjs753@gmail.com>
2025-08-10 21:15:03 +0900

    ✨ [Feat] CartItem Entity 추가

---
+++ src/main/java/com/shoppingorderapi/domain/cartItem/CartItem.java
@@ -0,0 +1,49 @@
+package com.shoppingorderapi.domain.cartItem;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import com.shoppingorderapi.domain.cart.Cart;
+import com.shoppingorderapi.domain.product.Product;
+
+@Entity
+@Table(name = "cart_items")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CartItem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "product_id", nullable = false)
+	private Product product;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "cart_id", nullable = false)
+	private Cart cart;
+
+	private int quantity;
+
+	@Builder
+	public CartItem(Product product, Cart cart, int quantity) {
+		this.product = product;
+		this.cart = cart;
+		this.quantity = quantity;
+	}
+
+	public void updateQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+}
