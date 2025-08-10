0da84f7becb4a188f764ebf232a1bcee5a5066aa
sinyoung0403 <tlstjs753@gmail.com>
2025-08-10 21:14:38 +0900

    ✨ [Feat] 삭제 쿼리 구현

--- src/main/java/com/shoppingorderapi/domain/cart/CartRepository.java
+++ src/main/java/com/shoppingorderapi/domain/cart/CartRepository.java
@@ -2,6 +2,10 @@ package com.shoppingorderapi.domain.cart;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.shoppingorderapi.common.repository.BaseRepository;
 
 public interface CartRepository extends BaseRepository<Cart, Long> {
@@ -9,4 +13,11 @@ public interface CartRepository extends BaseRepository<Cart, Long> {
 	Optional<Cart> findByUser_Id(Long userId);
 
 	boolean existsByUser_Id(Long userId);
+
+	@Modifying
+	@Query(
+		"DELETE FROM Cart c "
+			+ "WHERE c.user.id = :userId"
+	)
+	void deleteByUserId(@Param("userId") Long userId);
 }
