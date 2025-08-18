3dbba619c5bf557f6feac1b2229feb05f664ea6a
sinyoung0403 <tlstjs753@gmail.com>
2025-08-18 22:34:54 +0900

    ✨ [Feat] Order 일부 구현

---
+++ src/main/java/com/shoppingorderapi/domain/order/OrderRepository.java
@@ -0,0 +1,6 @@
+package com.shoppingorderapi.domain.order;
+
+import com.shoppingorderapi.common.repository.BaseRepository;
+
+public interface OrderRepository extends BaseRepository<Order, Long> {
+}
