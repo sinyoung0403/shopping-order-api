1770035b9afe68fc73431a8f906a65fe3c1fdaa7
sinyoung0403 <tlstjs753@gmail.com>
2025-08-18 22:35:51 +0900

    ✨ [Feat] OrderItemRepository 구현

---
+++ src/main/java/com/shoppingorderapi/domain/orderItem/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.shoppingorderapi.domain.orderItem;
+
+import java.util.Optional;
+
+import com.shoppingorderapi.common.repository.BaseRepository;
+
+public interface OrderItemRepository extends BaseRepository<OrderItem, Long> {
+
+}
