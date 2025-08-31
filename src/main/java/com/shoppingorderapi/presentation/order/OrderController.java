6fbfa9ce7780944d4d7d65f5451ffb2c635f8bab
sinyoung0403 <tlstjs753@gmail.com>
2025-08-31 16:53:19 +0900

    ♻️ [Refactor] 엔드포인트 경로 일관성

--- src/main/java/com/shoppingorderapi/presentation/order/OrderController.java
+++ src/main/java/com/shoppingorderapi/presentation/order/OrderController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +29,7 @@ import com.shoppingorderapi.domain.order.dto.response.FindOrderResponseDto;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/me")
 public class OrderController {
 
 	private final OrderService orderService;
