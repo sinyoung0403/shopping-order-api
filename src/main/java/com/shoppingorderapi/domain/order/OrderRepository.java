74feb85f90cf30f6885ecc1c3ec9d2a0a284dd3f
sinyoung0403 <tlstjs753@gmail.com>
2025-08-31 16:52:26 +0900

    ♻️ [Refactor] @param 누락 수정

--- src/main/java/com/shoppingorderapi/domain/order/OrderRepository.java
+++ src/main/java/com/shoppingorderapi/domain/order/OrderRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.shoppingorderapi.common.repository.BaseRepository;
 import com.shoppingorderapi.domain.order.dto.response.FindAllOrderResponseDto;
@@ -18,5 +19,5 @@ public interface OrderRepository extends BaseRepository<Order, Long> {
 			"FROM Order o " +
 			"WHERE o.user.id = :userId"
 	)
-	Page<FindAllOrderResponseDto> findAllOrderByUserId(Long userId, Pageable pageable);
+	Page<FindAllOrderResponseDto> findAllOrderByUserId(@Param("userId") Long userId, Pageable pageable);
 }
