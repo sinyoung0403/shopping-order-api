d4f59fd6fbba975271785dea7e3f14fc83ce308e
sinyoung0403 <tlstjs753@gmail.com>
2025-09-06 17:51:19 +0900

    ✨ [Feat] Spring Security User Details 관련 설정 구현 - UserDetails 구현 - UserDetails Service 구현(권한매핑)

---
+++ src/main/java/com/shoppingorderapi/application/auth/CustomUserDetailsService.java
@@ -0,0 +1,46 @@
+package com.shoppingorderapi.application.auth;
+
+import java.util.Collection;
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.shoppingorderapi.common.exception.CustomException;
+import com.shoppingorderapi.common.exception.ErrorCode;
+import com.shoppingorderapi.domain.user.User;
+import com.shoppingorderapi.infra.persistence.jpa.UserRepository;
+import com.shoppingorderapi.presentation.security.CustomUserDetails;
+
+/**
+ * 사용자 로드 → UserDetails 로 변환
+ */
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	@Override
+	@Transactional(readOnly = true)
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+		// 1) 유저 조회
+		User user = userRepository.findByEmail(email)
+			.orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
+
+		// 2) 권한 매핑
+		Collection<? extends GrantedAuthority> authorities =
+			List.of(new SimpleGrantedAuthority(user.getRole().name()));
+
+		// 3) CustomUserDetails 생성
+		return CustomUserDetails.of(user.getId(), email, authorities);
+	}
+}
