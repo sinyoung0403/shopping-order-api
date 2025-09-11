9afdf7e058feda2002def943a6d58323f782b78c
sinyoung0403 <tlstjs753@gmail.com>
2025-09-12 00:38:25 +0900

    ✨ [Feat] 권한기반 어노테이션 에러 ResposneEntity 반환을 위해 Handler 추가

--- src/main/java/com/shoppingorderapi/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/shoppingorderapi/common/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.shoppingorderapi.common.response.BaseResponse;
@@ -43,5 +44,34 @@ public class GlobalExceptionHandler {
 				ErrorCode.INTERNAL_SERVER_ERROR.getMessage(),
 				ErrorCode.INTERNAL_SERVER_ERROR.getCode()
 			));
+	}
+
+	/**
+	 * Security Role 보안 예외 처리
+	 */
+
+	@ExceptionHandler({
+		org.springframework.security.access.AccessDeniedException.class,
+		org.springframework.security.authorization.AuthorizationDeniedException.class
+	})
+	@ResponseStatus(HttpStatus.FORBIDDEN)
+	public BaseResponse handleAccessDenied(Exception e) {
+		return BaseResponse.error(
+			ErrorCode.FORBIDDEN.getMessage(),
+			ErrorCode.FORBIDDEN.getCode()
+		);
+	}
+
+	/**
+	 * Security Role 보안 예외 처리
+	 */
+
+	@ExceptionHandler(org.springframework.security.core.AuthenticationException.class)
+	@ResponseStatus(HttpStatus.UNAUTHORIZED)
+	public BaseResponse handleAuth(Exception e) {
+		return BaseResponse.error(
+			ErrorCode.UNAUTHORIZED.getMessage(),
+			ErrorCode.UNAUTHORIZED.getCode()
+		);
 	}
 }
