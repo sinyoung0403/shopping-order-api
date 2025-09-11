8844ccf778d1696a0d965295f4aba05104f06038
sinyoung0403 <tlstjs753@gmail.com>
2025-09-12 00:38:48 +0900

    ✨ [Feat] UserRole 에 String 값을 UserRole 로 바꾸는 정적메서드 추가

--- src/main/java/com/shoppingorderapi/domain/user/UserRole.java
+++ src/main/java/com/shoppingorderapi/domain/user/UserRole.java
@@ -1,5 +1,9 @@
 package com.shoppingorderapi.domain.user;
 
 public enum	UserRole {
-	USER, OWNER
+	USER, OWNER;
+
+	public static UserRole of(String role) {
+		return UserRole.valueOf(role.toUpperCase());
+	}
 }
